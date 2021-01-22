f64ae34c240438afd07a84675fd4cf19ebf0f190
mlum123 <michellelum123@gmail.com>
2021-01-21 19:49:58 -0800

    restyled to space theme

--- GameScene.js
+++ GameScene.js
@@ -5,15 +5,92 @@ class GameScene extends Phaser.Scene {
     super({ key: "GameScene" });
   }
 
+  preload() {
+    this.load.image("astronaut", "./img/005-space suit.png");
+  }
+
   create() {
-    // gradient background
-    const background = this.add.graphics();
-    background.fillGradientStyle(0x5ab9ea, 0x84ceeb, 0xc1c8e4, 0x8860d0, 1);
-    background.fillRect(
-      0,
-      0,
-      gameState.gameOptions.width,
-      gameState.gameOptions.height
+    this.physics.world.setBounds(0, 0, 550, 710);
+
+    // create player
+    gameState.player = this.physics.add
+      .sprite(100, 450, "astronaut")
+      .setScale(0.2);
+    gameState.player.setCollideWorldBounds(true);
+    gameState.player.setBounce(1);
+    gameState.player.body.checkCollision.up = false;
+    gameState.player.body.checkCollision.left = false;
+    gameState.player.body.checkCollision.right = false;
+
+    // create randomly placed platforms, with spacing that increases with score
+    gameState.platforms = this.physics.add.group({
+      allowGravity: false,
+      immovable: true,
+    });
+
+    for (let i = 0; i < 8; i++) {
+      const randomX = Math.floor(Math.random() * 400) + 50;
+      gameState.platforms.create(randomX, i * 100, "star").setScale(0.3);
+    }
+
+    // player-platform collision
+    this.physics.add.collider(gameState.player, gameState.platforms);
+
+    // define cursors from keyboard
+    gameState.cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+    // factor in keyboard input
+    if (gameState.cursors.left.isDown) {
+      gameState.player.setVelocityX(-240);
+      gameState.player.flipX = true;
+    } else if (gameState.cursors.right.isDown) {
+      gameState.player.setVelocityX(240);
+      gameState.player.flipX = false;
+    } else {
+      gameState.player.setVelocityX(0);
+    }
+
+    if (gameState.scoreDisplay !== undefined) {
+      gameState.scoreDisplay.destroy();
+    }
+
+    gameState.scoreDisplay = this.add.text(
+      50,
+      50,
+      `score: ${gameState.score}`,
+      {
+        fontSize: "25px",
+        fontFamily: "Poppins, sans-serif",
+      }
     );
+
+    // have player bounce off of platforms to move up
+    if (gameState.player.body.touching.down) {
+      gameState.player.setVelocityY(-500);
+      gameState.score += 1;
+      this.cameras.main.shake(100, 0.004);
+    }
+
+    // move platforms downward as player progresses upward
+    if (gameState.player.body.y < gameState.gameOptions.height / 2) {
+      gameState.platforms.children.iterate(updateY, this);
+    }
+  }
+}
+
+// updateY function moves platforms downward off the screen, then repositions them to show new platforms
+function updateY(platform) {
+  // keep player in center of screen
+  let delta = Math.floor(gameState.gameOptions.height / 2) - gameState.player.y;
+
+  if (delta > 0) {
+    platform.y += delta / 30; // ensures delta is small enough
+  }
+
+  if (platform.y > gameState.gameOptions.height) {
+    platform.y = -(0.3 * platform.height);
+    platform.x = Math.floor(Math.random() * 400) + 50;
   }
 }
--- StartScene.js
+++ StartScene.js
@@ -4,43 +4,52 @@ class StartScene extends Phaser.Scene {
     super({ key: "StartScene " });
   }
 
-  create() {
-    // gradient background
-    const background = this.add.graphics();
-    background.fillGradientStyle(0x5ab9ea, 0x84ceeb, 0xc1c8e4, 0x8860d0, 1);
-    background.fillRect(
-      0,
-      0,
-      gameState.gameOptions.width,
-      gameState.gameOptions.height
-    );
+  preload() {
+    this.load.image("star", "./img/006-falling star.png");
+  }
 
+  create() {
     // title text
-    const title = this.add.text(560, 200, "doodle jump", {
+    this.add.text(560, 180, "doodle jump", {
       fontSize: "50px",
       fontFamily: "Poppins, sans-serif",
     });
+    this.add.text(610, 250, "in outer space", {
+      fontFamily: "Poppins, sans-serif",
+      fontSize: "30px",
+      fontStyle: "italic",
+    });
 
     // play button
-    const playButton = this.add.rectangle(720, 350, 105, 60);
-    playButton.setStrokeStyle(4, 0xffffff);
+    const playButton = this.add.rectangle(720, 360, 105, 60, 0xffffff);
 
-    const playText = this.add.text(688, 330, "play", {
+    this.add.text(688, 340, "play", {
       fontSize: "30px",
       fontFamily: "Poppins, sans-serif",
+      color: "0x000",
     });
 
     playButton.setInteractive();
     // make play button fade in and out on hover
     playButton.on("pointerover", () => {
-      this.tweens.add({
-        targets: [playButton, playText],
-        duration: 1200,
-        alpha: 0.3,
-        yoyo: true,
-        repeat: -1,
-        ease: "Sine.easeInOut",
-      });
+      playButton.setScale(1.2);
+    });
+
+    playButton.on("pointerout", () => {
+      playButton.setScale(1);
+    });
+
+    // shooting star icon
+    const star = this.add
+      .image(725, 410, "star")
+      .setScale(0.2)
+      .setFlip(true, true);
+    this.tweens.add({
+      targets: star,
+      angle: 30,
+      yoyo: true,
+      repeat: -1,
+      ease: "Sine.easeInOut",
     });
 
     // show next scene when user clicks on play button
